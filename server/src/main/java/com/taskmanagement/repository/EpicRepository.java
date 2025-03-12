9653e2e5a8062484e1aeb83e5cb6be04dffecd30
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2025-03-12 18:01:57 +0200

    implemented basic custom methods in EpicsRepository interface

--- server/src/main/java/com/taskmanagement/repository/EpicRepository.java
+++ server/src/main/java/com/taskmanagement/repository/EpicRepository.java
@@ -1,12 +1,15 @@
 package com.taskmanagement.repository;
 
 import com.taskmanagement.model.Epic;
+import com.taskmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EpicRepository extends JpaRepository<Epic, UUID> {
-
+    List<Epic> findByOwner(User owner);
+    List<Epic> findByName(String name);
 }
